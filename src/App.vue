997640d53abae463075e7749483944a682585b80
gayoung106 <xjongamx@kyonggi.ac.kr>
2024-02-04 12:24:30 +0900

    feat: composition api 적용해보기

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Gayoung</div>
+  <div class="name">{{ name }}</div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const name = "gayoung";
+
+    return {
+      name,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: blue;
+}
+</style>
